070624a29cf26b532d7f2c0df301806df8e2b0e1
Pete <11158905+Pittawat2542@users.noreply.github.com>
2020-03-30 08:58:09 +0700

    [ADD] ARScene Demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,158 @@
+import 'dart:math' as math;
+import 'package:arkit_plugin/arkit_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: HelloWorldPage()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class HelloWorldPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HelloWorldPagState createState() => _HelloWorldPagState();
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class _HelloWorldPagState extends State<HelloWorldPage> {
+  ARKitController arkitController;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void dispose() {
+    arkitController?.dispose();
+    super.dispose();
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
+  Widget build(BuildContext context) => Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('ARKit in Flutter'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Container(
+        child: ARKitSceneView(onARKitViewCreated: onARKitViewCreated),
+      ));
+
+  void onARKitViewCreated(ARKitController arkitController) {
+    this.arkitController = arkitController;
+
+    this.arkitController.add(_createSphere());
+    this.arkitController.add(_createPlane());
+    this.arkitController.add(_createText());
+    this.arkitController.add(_createBox());
+    this.arkitController.add(_createCylinder());
+    this.arkitController.add(_createCone());
+    this.arkitController.add(_createPyramid());
+    this.arkitController.add(_createTube());
+    this.arkitController.add(_createTorus());
+    this.arkitController.add(_createCapsule());
+  }
+
+  ARKitNode _createSphere() => ARKitNode(
+        geometry:
+            ARKitSphere(materials: _createRandomColorMaterial(), radius: 0.04),
+        position: vector.Vector3(-0.1, -0.1, -0.5),
+      );
+
+  ARKitNode _createPlane() {
+    final plane = ARKitPlane(
+      width: 1,
+      height: 1,
+      materials: [
+        ARKitMaterial(
+          transparency: 0.5,
+          diffuse: ARKitMaterialProperty(color: Colors.white),
+        )
+      ],
     );
+    return ARKitNode(
+      geometry: plane,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+  }
+
+  ARKitNode _createText() {
+    final text = ARKitText(
+      text: 'Flutter',
+      extrusionDepth: 1,
+      materials: [
+        ARKitMaterial(
+          diffuse: ARKitMaterialProperty(color: Colors.blue),
+        )
+      ],
+    );
+    return ARKitNode(
+      geometry: text,
+      position: vector.Vector3(-0.3, 0.3, -1.4),
+      scale: vector.Vector3(0.02, 0.02, 0.02),
+    );
+  }
+
+  ARKitNode _createBox() => ARKitNode(
+        geometry: ARKitBox(
+            width: 0.06,
+            height: 0.06,
+            length: 0.06,
+            chamferRadius: 0.01,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(-0.1, 0, -0.5),
+      );
+
+  ARKitNode _createCylinder() => ARKitNode(
+        geometry: ARKitCylinder(
+            radius: 0.05,
+            height: 0.09,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(-0.1, 0.1, -0.5),
+      );
+
+  ARKitNode _createCone() => ARKitNode(
+        geometry: ARKitCone(
+            topRadius: 0,
+            bottomRadius: 0.05,
+            height: 0.09,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(0, -0.1, -0.5),
+      );
+
+  ARKitNode _createPyramid() => ARKitNode(
+        geometry: ARKitPyramid(
+            width: 0.09,
+            height: 0.09,
+            length: 0.09,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(0, -0.05, -0.5),
+      );
+
+  ARKitNode _createTube() => ARKitNode(
+        geometry: ARKitTube(
+            innerRadius: 0.03,
+            outerRadius: 0.05,
+            height: 0.09,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(0.1, 0.1, -0.5),
+      );
+
+  ARKitNode _createTorus() => ARKitNode(
+        geometry: ARKitTorus(
+            ringRadius: 0.04,
+            pipeRadius: 0.02,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(0.1, -0.1, -0.5),
+      );
+
+  ARKitNode _createCapsule() => ARKitNode(
+        geometry: ARKitCapsule(
+            capRadius: 0.02,
+            height: 0.06,
+            materials: _createRandomColorMaterial()),
+        position: vector.Vector3(0.1, 0, -0.5),
+      );
+
+  List<ARKitMaterial> _createRandomColorMaterial() {
+    return [
+      ARKitMaterial(
+        lightingModelName: ARKitLightingModel.physicallyBased,
+        diffuse: ARKitMaterialProperty(
+          color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+              .withOpacity(1.0),
+        ),
+      )
+    ];
   }
 }
